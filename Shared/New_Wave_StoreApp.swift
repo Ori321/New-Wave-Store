586bc43b523a4b3e61c08361d5264bbfd4257c20
Ori123 <ori.orbach@koheletyeshiva.org>
2022-11-21 07:36:59 -0500

    Initial Commit

---
+++ Shared/New_Wave_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Wave_StoreApp.swift
+//  Shared
+//
+//  Created by Ori Orbach (student LM) on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct New_Wave_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
